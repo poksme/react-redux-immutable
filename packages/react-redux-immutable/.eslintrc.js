e7a5d86b105353c9f3596123eca17ef52b3dcb4a
Bertrand Boustany <poksme@gmail.com>
2018-11-03 17:12:22 +0900

    1.1.1 Lerna Restructure (#4)

--- packages/react-redux-immutable/.eslintrc.js
+++ packages/react-redux-immutable/.eslintrc.js
@@ -1,4 +1,3 @@
-const { peerDependencies } = require('../package/package.json');
 const off = 0;
 const warn = 1;
 const error = 2;
@@ -10,9 +9,6 @@ module.exports = {
     sourceType: "module"
   },
   extends: ["airbnb", "plugin:import/errors", "plugin:import/warnings"],
-  settings: {
-    "import/core-modules": Object.keys(peerDependencies)
-  },
   plugins: ["import"],
   rules: {
     "import/no-deprecated": warn
