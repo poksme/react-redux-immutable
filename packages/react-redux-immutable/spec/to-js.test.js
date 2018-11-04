4011da8f5093266402ae2f655b223fe903f4b57d
Bertrand Boustany <bertrand@boustany.pro>
2018-11-04 13:19:38 +0900

    Better build rule to avoid duplicate README and LICENSE. Descriptive UT

--- packages/react-redux-immutable/spec/to-js.test.js
+++ packages/react-redux-immutable/spec/to-js.test.js
@@ -1,53 +1,55 @@
 import { List } from 'immutable';
 import { mapComponentProps } from '../src/to-js';
 
-test('does not break on empty call', () => {
-  expect(mapComponentProps()).toEqual({});
-});
-
-test('converts immutable list', () => {
-  expect(
-    mapComponentProps({
-      pets: List(['La Stoune', 'Bibou']),
-    }),
-  ).toEqual({
-    pets: ['La Stoune', 'Bibou'],
+describe('mapComponentProps', () => {
+  test('does not break on empty call', () => {
+    expect(mapComponentProps()).toEqual({});
   });
-});
 
-test('does not convert string', () => {
-  expect(
-    mapComponentProps({
+  test('converts immutable list', () => {
+    expect(
+      mapComponentProps({
+        pets: List(['La Stoune', 'Bibou']),
+      }),
+    ).toEqual({
+      pets: ['La Stoune', 'Bibou'],
+    });
+  });
+
+  test('does not convert string', () => {
+    expect(
+      mapComponentProps({
+        name: 'Thibault',
+      }),
+    ).toEqual({
       name: 'Thibault',
-    }),
-  ).toEqual({
-    name: 'Thibault',
+    });
   });
-});
 
-test('converts mixed props', () => {
-  expect(
-    mapComponentProps({
-      name: 'Thibault',
-      pets: List(['La Stoune', 'Bibou']),
-    }),
-  ).toEqual({
-    name: 'Thibault',
-    pets: ['La Stoune', 'Bibou'],
-  });
-});
-
-test('ignores props from ignore list', () => {
-  expect(
-    mapComponentProps(
-      {
+  test('converts mixed props', () => {
+    expect(
+      mapComponentProps({
         name: 'Thibault',
         pets: List(['La Stoune', 'Bibou']),
-      },
-      ['pets'],
-    ),
-  ).toEqual({
-    name: 'Thibault',
-    pets: List(['La Stoune', 'Bibou']),
+      }),
+    ).toEqual({
+      name: 'Thibault',
+      pets: ['La Stoune', 'Bibou'],
+    });
+  });
+
+  test('ignores props from ignore list', () => {
+    expect(
+      mapComponentProps(
+        {
+          name: 'Thibault',
+          pets: List(['La Stoune', 'Bibou']),
+        },
+        ['pets'],
+      ),
+    ).toEqual({
+      name: 'Thibault',
+      pets: List(['La Stoune', 'Bibou']),
+    });
   });
 });
